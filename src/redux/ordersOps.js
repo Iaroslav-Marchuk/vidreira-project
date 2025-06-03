8df3ca9504bfe3b0c8f1212ee29256a34e6ec56d
iaroslav_marchuk <jarik.13a@gmail.com>
2025-06-03 23:03:09 +0100

    add work

---
+++ src/redux/ordersOps.js
@@ -0,0 +1,57 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://luck-plume-ease.glitch.me";
+
+export const fetchOrders = createAsyncThunk(
+  "orders/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("/orders");
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addOrder = createAsyncThunk(
+  "orders/addOrder",
+  async (
+    {
+      local: { zona, operator, date },
+      EP,
+      cliente,
+      units: [
+        { categoria, type, temper, sizeX, sizeY, sizeZ, quantity, reason },
+      ],
+    },
+    thunkAPI
+  ) => {
+    try {
+      const response = await axios.post("/orders", {
+        local: { zona, operator, date },
+        EP,
+        cliente,
+        units: [
+          { categoria, type, temper, sizeX, sizeY, sizeZ, quantity, reason },
+        ],
+      });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteOrder = createAsyncThunk(
+  "orders/deleteContact",
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/orders/${id}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
